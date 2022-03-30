35c1bf0b0ab8da8366946ba931a4df45b5bebc6c
Naveen-2 <naveen2k15cse@gmail.com>
2022-03-31 00:17:31 +0530

    Data can be retrieved from DB

---
+++ JDBC/src/com/PayrollExceptions.java
@@ -0,0 +1,13 @@
+package com;
+
+public class PayrollExceptions extends Throwable {
+    enum ExceptionType{
+        CONNECTION_PROBLEM, RETRIEVAL_PROBLEM;
+    }
+
+    ExceptionType type;
+    PayrollExceptions(String message, ExceptionType type){
+        super(message);
+        this.type = type;
+    }
+}
